4e603bdb2c0a9531c229c4fbb8a1dc5fd68c3489
Haoyang <hygzhu@gmail.com>
2018-04-04 19:26:19 -0400

    Works

--- script.user.js
+++ script.user.js
@@ -2,7 +2,7 @@
 // @name         Reddit April Fools 2018 Trust Script
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Updates Usernames with /r/CircleOfTrust flairs in the reddit inbox (Only works for accounts with old profile at the moment)
+// @description  Check yourself before you shrek yourself
 // @author       hygzhu
 // @match        *.reddit.com/message/inbox/*
 // @grant        none
@@ -13,7 +13,7 @@
     // @run-at document-end
     let authors = document.getElementsByClassName("author");
     for(let i = 0; i < authors.length; i++){
-        let profile_link = authors[i].href;
+        let profile_link = authors[i].href + "/overview";
         let comment_permalink = "";
         let profile = new XMLHttpRequest();
         profile.onload = function () {
@@ -31,8 +31,24 @@
                 permalinked_comment.onload = function(){
                     if(permalinked_comment.readyState == 4){
                         let new_doc = parser.parseFromString(permalinked_comment.responseText, "text/html");
-                        let flairs = new_doc.getElementsByClassName("author submitter");
-                        authors[i].innerHTML += ` (Flair: ${flairs[0].parentNode.getElementsByClassName("flair")[0].innerHTML})`;
+                        let flairs = new_doc.getElementsByClassName("submitter");
+                        let flairs2 = new_doc.getElementsByClassName("author");
+                        let all_flairs = [];
+                        all_flairs = Array.prototype.concat.apply(all_flairs, flairs);
+                        all_flairs = Array.prototype.concat.apply(all_flairs, flairs2);
+                        all_flairs= all_flairs.filter(x => x.href == authors[i].href);
+                        console.log(all_flairs);
+                        console.log(comment_permalink);
+                        try{
+                        authors[i].innerHTML += ` (Flair: ${all_flairs[0].parentNode.getElementsByClassName("flair")[0].innerHTML})`;
+                        } catch (e){
+                        authors[i].innerHTML += ` (Unable to Retrieve flair)`;
+                        }
+                        if(authors[i].innerHTML.includes("∅")){
+                            authors[i].style.background = "#E50002";
+                        }else if(!authors[i].innerHTML.includes("(Unable to Retrieve flair)")){
+                            authors[i].style.background = "#5FE500";
+                        }
                     }
                 };
                 console.log(comment_permalink);
